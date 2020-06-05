cd7db233f9f5cb3a2ed6fc262a0a21386378ac80
DevHuzi <hux3fa@gmail.com>
2020-06-05 14:01:44 +1000

    adding project files

---
+++ genBgFun.js
@@ -0,0 +1,14 @@
+var head = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
+
+
+function genBackground(){
+    body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+    head.textContent = "Background Color Gradient from: " + color1.value + " - " + color2.value + " ;";
+}
+
+
+color1.addEventListener("input", genBackground);
+color2.addEventListener("input", genBackground);
